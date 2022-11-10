6f0dfd1c5bbb925e5070bcee94e6135e1801563a
MohamadouTogo <mtogo884@gmail.com>
2022-11-10 13:15:01 +0000

    gestion GS

---
+++ application/controllers/Groups_sang.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Groups_sang extends CI_Controller {
+    public function __construct() {
+        parent:: __construct();
+        $this->load->model('Groups_sang_model');
+    }
+
+	public function index() {
+        $data['result'] = $this->Groups_sang_model->getAllData();
+		return $this->load->view('Admin/include/header')
+                            ->view('Admin/include/sidebar')
+                            ->view('Admin/fontawesome',$data)
+                            ->view('Admin/include/footer');
+    }
+
+    public function create() {
+        $this->Groups_sang_model->createData();
+        redirect("Groups_sang");
+    }
+
+    public function edit($id) {
+        $data['row'] = $this->Groups_sang_model->getData($id);
+        $this->load->view('Admin/include/header')
+                    ->view('Admin/Groups_sangEdit', $data)
+                    ->view('Admin/include/sidebar')
+                    ->view('Admin/include/footer');
+    }
+
+    public function update($id) {
+        $this->Groups_sang_model->updateData($id);
+        redirect("Groups_sang");
+    }
+
+    public function delete($id) {
+        $this->Groups_sang_model->deleteData($id);
+        redirect("Groups_sang");
+    }
+    
+}
